6dc8197793b3fe1abe1cab5617683059442fa2ce
milan purbia <53895271+milanpurbia26@users.noreply.github.com>
2024-03-26 11:05:45 +0530

    Update firstMissingNumber.java
    refracted

--- firstMissingNumber.java
+++ firstMissingNumber.java
@@ -1,2 +1,26 @@
-public class firstMissingNumber {
+import java.util.Arrays;
+
+class firstMissingPositive {
+
+    public static void main(String[] args) {
+        //smallest positive number is 1
+        int nums[] = {0, 1, 2};
+        int smallestPositive = 1;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == smallestPositive) {
+                smallestPositive += 1;
+            } else if (nums[i] > smallestPositive) {
+                break;
+            }
+
+        }
+        System.out.println(smallestPositive);
+    }
+
 }
+
+
+
+
+
