f6f62f6bf51b8c090b17a10631653dd09cfc472d
milan purbia <53895271+milanpurbia26@users.noreply.github.com>
2024-04-11 23:52:47 +0530

    day 17 commit

---
+++ RemoveKDigit.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class RemoveKDigit {
+    public static void main(String[] args) {
+        String  num = "1432219";
+                int k = 3;
+          String ans = RemoveKDigits(num,k);
+        System.out.println(" after removing K digit " + ans);
+    }
+
+    private static String RemoveKDigits(String num,int k) {
+        if(num.length() ==0)
+            return "0";
+        Stack<Character> st = new Stack<>();
+        for(char ch : num.toCharArray())
+        {
+            while (!st.isEmpty() && k>0 && st.peek() > ch)
+            {
+                st.pop();
+                k--;
+            }
+            st.push(ch);
+        }
+        while (k>0 && !st.isEmpty())
+        {
+            st.pop();
+            k--;
+        }
+
+        StringBuilder result = new StringBuilder();
+        while(!st.isEmpty())
+        {
+            result.insert(0,st.pop());
+        }
+        return result.length() == 0 ? "0" : result.toString();
+    }
+}
